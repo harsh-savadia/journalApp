976bac3926b59c40d934de8f12b23c089a45ee0b
Harsh Savadia <harshb.savadia@gmail.com>
2024-10-18 00:28:07 +0530

    JUnit & Mockito

---
+++ src/test/java/com/example/journalApp/Service/UserServiceTests.java
@@ -0,0 +1,21 @@
+package com.example.journalApp.Service;
+
+import com.example.journalApp.Repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+public class UserServiceTests {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    public void testFindByUserName() {
+        assertNotNull(userRepository.findByUserName("Harsh"));
+    }
+}
