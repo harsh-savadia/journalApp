621420c63496ab5d88af6a7ef38562ea3dea2179
Harsh Savadia <harshb.savadia@gmail.com>
2024-10-19 22:51:32 +0530

    Logging in Spring Boot - Logback to SLF4J

--- src/main/java/com/example/journalApp/Service/UserService.java
+++ src/main/java/com/example/journalApp/Service/UserService.java
@@ -2,7 +2,10 @@ package com.example.journalApp.Service;
 
 import com.example.journalApp.Entity.User;
 import com.example.journalApp.Repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.bson.types.ObjectId;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -13,26 +16,39 @@ import java.util.List;
 import java.util.Optional;
 
 @Component
+@Slf4j
 public class UserService {
 
     @Autowired
     private UserRepository userRepository;
 
+//    private static final Logger logger = LoggerFactory.getLogger(UserService.class);
+
     private static final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
-    public void saveNewUser(User user) {
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
-        user.setRoles(Arrays.asList("USER"));
-        userRepository.save(user);
+    public boolean saveNewUser(User user) {
+        try {
+            user.setPassword(passwordEncoder.encode(user.getPassword()));
+            user.setRoles(Arrays.asList("USER"));
+            userRepository.save(user);
+            return true;
+        } catch (Exception e) {
+            log.error("Error occurred for {} :",user.getUserName(), e);
+            log.warn("hahahahahahahah");
+            log.info("hahahahahahahah");
+            log.debug("hahahahahahahah");
+            log.trace("hahahahahahahah");
+            return false;
+        }
     }
 
     public void saveAdmin(User user) {
         user.setPassword(passwordEncoder.encode(user.getPassword()));
-        user.setRoles(Arrays.asList("USER","ADMIN"));
+        user.setRoles(Arrays.asList("USER", "ADMIN"));
         userRepository.save(user);
     }
 
-    public void saveUser(User user){
+    public void saveUser(User user) {
         userRepository.save(user);
     }
 
